0d38dfc09f615641ba3e1de08c05e93388e970b4
Frederic Samvura <fsamvura@gmail.com>
2022-06-16 00:22:37 -0400

    Ajout Template Ekival

---
+++ updev_ekivalapp-dev/app.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- <Html :class="`${theme === 'dark' ? 'dark' : ''}`" :lang="locale"> -->
+  <Html :lang="locale">
+
+  <Body>
+    <NuxtLayout>
+      <NuxtPage />
+      <ClientOnly>
+        <notifications />
+      </ClientOnly>
+    </NuxtLayout>
+  </Body>
+
+  </Html>
+</template>
+
+<script lang="ts" setup>
+import { AppSetup } from './utils/app'
+import { ITheme } from './utils/theme'
+AppSetup()
+const theme = useState<ITheme>('theme.current')
+const locale = useState<string>('locale.setting')
+</script>
